ab805efff4d3250395cf0d387b8ca2f5f9031bbc
Alex Suraci <asuraci@pivotal.io>
2015-08-05 16:31:19 -0700

    fix etcd helper err check
    Signed-off-by: Jen Spinney <jen.spinney@hp.com>

--- db/etcd/internal/etcd_helpers/getters.go
+++ db/etcd/internal/etcd_helpers/getters.go
@@ -3,16 +3,17 @@ package etcd_helpers
 import (
 	"github.com/cloudfoundry-incubator/bbs/db/etcd"
 	"github.com/cloudfoundry-incubator/bbs/models"
-	"github.com/cloudfoundry/storeadapter"
+	etcdclient "github.com/coreos/go-etcd/etcd"
 
 	. "github.com/onsi/gomega"
 )
 
 func (t *ETCDHelper) GetInstanceActualLRP(lrpKey *models.ActualLRPKey) (*models.ActualLRP, error) {
 	resp, err := t.etcdClient.Get(etcd.ActualLRPSchemaPath(lrpKey.ProcessGuid, lrpKey.Index), false, false)
-	if err == storeadapter.ErrorKeyNotFound {
+	if etcdErr, ok := err.(*etcdclient.EtcdError); ok && etcdErr.ErrorCode == etcd.ETCDErrKeyNotFound {
 		return &models.ActualLRP{}, models.ErrResourceNotFound
 	}
+
 	Expect(err).NotTo(HaveOccurred())
 
 	var lrp models.ActualLRP
