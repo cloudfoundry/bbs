f6c529a85b1e0d2f57195c840f293071bfc2cc4f
Luan Santos <lsantos@pivotal.io>
2015-09-01 11:52:11 -0700

    Re-install protoc binary upon generating protos
    Signed-off-by: Andrew Edgar <aedgar@ca.ibm.com>

---
+++ scripts/generate_protos.sh
@@ -0,0 +1,7 @@
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  echo "OSX workstation detected, updating protobuf binaries..."
+  rm $GOPATH/bin/protoc-gen-gogoslick
+  go install -v github.com/gogo/protobuf/protoc-gen-gogoslick
+fi
+
+protoc --proto_path=$GOPATH/src:$GOPATH/src/github.com/gogo/protobuf/protobuf/:. --gogoslick_out=. *.proto
