b7657b00ac90c08c67cf941b5116e5a6f56983c6
Andrew Edgar <aedgar@ca.ibm.com>
2015-12-31 11:22:38 -0600

    Moved createRequest into retry loop to fix retries
    [#110887808]
    
    Signed-off-by: Nick Calugar <ncalugar@pivotal.io>

--- client.go
+++ client.go
@@ -667,11 +667,12 @@ func (c *client) doEvacRequest(route string, defaultKeepContainer bool, request 
 
 func (c *client) doRequest(requestName string, params rata.Params, queryParams url.Values, requestBody, responseBody proto.Message) error {
 	var err error
-	request, err := c.createRequest(requestName, params, queryParams, requestBody)
-	if err != nil {
-		return err
-	}
+
 	for attempts := 0; attempts < 10; attempts++ {
+		request, err := c.createRequest(requestName, params, queryParams, requestBody)
+		if err != nil {
+			return err
+		}
 		err = c.do(request, responseBody)
 		if err != nil {
 			time.Sleep(500 * time.Millisecond)
