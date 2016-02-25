c3aa0244d5e4053c855c5bc4804f9e07982a8a51
Luan Santos <lsantos@pivotal.io>
2016-02-24 18:31:59 -0800

    Support for DesiredLRP DB on SQL
    * lock for share on update/remove
    
    [#113854821]
    
    Signed-off-by: James Myers <jmyers@pivotal.io>

--- db/sqldb/desired_lrp_db.go
+++ db/sqldb/desired_lrp_db.go
@@ -3,6 +3,8 @@ package sqldb
 import (
 	"database/sql"
 	"encoding/json"
+	"fmt"
+	"strings"
 
 	"github.com/cloudfoundry-incubator/bbs/models"
 	"github.com/pivotal-golang/lager"
@@ -42,4 +44,217 @@ func (db *SQLDB) DesireLRP(logger lager.Logger, desiredLRP *models.DesiredLRP) e
 		}
 		return nil
 	})
+}
+
+func (db *SQLDB) DesiredLRPByProcessGuid(logger lager.Logger, processGuid string) (*models.DesiredLRP, error) {
+	row := db.db.QueryRow("SELECT * FROM desired_lrps WHERE process_guid = ?", processGuid)
+	return db.fetchDesiredLRP(logger, row)
+}
+
+func (db *SQLDB) DesiredLRPs(logger lager.Logger, filter models.DesiredLRPFilter) ([]*models.DesiredLRP, error) {
+	var rows *sql.Rows
+	var err error
+	if filter.Domain != "" {
+		rows, err = db.db.Query("SELECT * FROM desired_lrps WHERE domain = ?", filter.Domain)
+	} else {
+		rows, err = db.db.Query("SELECT * FROM desired_lrps")
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	results := []*models.DesiredLRP{}
+	for rows.Next() {
+		desiredLRP, err := db.fetchDesiredLRP(logger, rows)
+		if err != nil {
+			continue
+		}
+		results = append(results, desiredLRP)
+	}
+
+	return results, nil
+}
+
+func (db *SQLDB) DesiredLRPSchedulingInfos(logger lager.Logger, filter models.DesiredLRPFilter) ([]*models.DesiredLRPSchedulingInfo, error) {
+	var rows *sql.Rows
+	var err error
+	if filter.Domain != "" {
+		rows, err = db.db.Query(`
+		SELECT process_guid, domain, log_guid, annotation, instances, memory_mb,
+			disk_mb, rootfs, routes, modification_tag_epoch, modification_tag_index
+			FROM desired_lrps WHERE domain = ?`,
+			filter.Domain,
+		)
+	} else {
+		rows, err = db.db.Query(`
+		SELECT process_guid, domain, log_guid, annotation, instances, memory_mb,
+			disk_mb, rootfs, routes, modification_tag_epoch, modification_tag_index
+			FROM desired_lrps`,
+		)
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	results := []*models.DesiredLRPSchedulingInfo{}
+	for rows.Next() {
+		desiredLRPSchedulingInfo, err := db.fetchDesiredLRPSchedulingInfo(logger, rows)
+		if err != nil {
+			continue
+		}
+		results = append(results, desiredLRPSchedulingInfo)
+	}
+
+	return results, nil
+}
+
+func (db *SQLDB) UpdateDesiredLRP(logger lager.Logger, processGuid string, update *models.DesiredLRPUpdate) error {
+	return db.transact(logger, func(logger lager.Logger, tx *sql.Tx) error {
+		err := db.lockDesiredLRPByGuidForShare(logger, processGuid, tx)
+		if err != nil {
+			return err
+		}
+
+		setKeys := []string{}
+		setValues := []interface{}{}
+		if update.Annotation != nil {
+			setKeys = append(setKeys, "annotation = ?")
+			setValues = append(setValues, *update.Annotation)
+		}
+
+		if update.Instances != nil {
+			setKeys = append(setKeys, "instances = ?")
+			setValues = append(setValues, *update.Instances)
+		}
+
+		if update.Routes != nil {
+			routeData, err := json.Marshal(update.Routes)
+			if err != nil {
+				return models.ErrBadRequest
+			}
+			setKeys = append(setKeys, "routes = ?")
+			setValues = append(setValues, routeData)
+		}
+
+		if len(setKeys) == 0 {
+			return nil
+		}
+
+		setValues = append(setValues, processGuid)
+
+		query := fmt.Sprintf("UPDATE desired_lrps SET %s WHERE process_guid = ?", strings.Join(setKeys, ", "))
+		stmt, err := tx.Prepare(query)
+		if err != nil {
+			return db.convertSQLError(err)
+		}
+
+		_, err = stmt.Exec(setValues...)
+		if err != nil {
+			return db.convertSQLError(err)
+		}
+
+		return nil
+	})
+}
+
+func (db *SQLDB) RemoveDesiredLRP(logger lager.Logger, processGuid string) error {
+	return db.transact(logger, func(logger lager.Logger, tx *sql.Tx) error {
+		err := db.lockDesiredLRPByGuidForShare(logger, processGuid, tx)
+		if err != nil {
+			return err
+		}
+
+		_, err = tx.Exec("DELETE FROM desired_lrps WHERE process_guid = ?", processGuid)
+		if err != nil {
+			panic(err)
+			return db.convertSQLError(err)
+		}
+
+		return nil
+	})
+}
+
+func (db *SQLDB) lockDesiredLRPByGuidForShare(logger lager.Logger, processGuid string, tx *sql.Tx) error {
+	row := tx.QueryRow("SELECT count(*) FROM desired_lrps WHERE process_guid = ? LOCK IN SHARE MODE", processGuid)
+	var count int
+	err := row.Scan(&count)
+	if err != nil {
+		return db.convertSQLError(err)
+	}
+	if count == 0 {
+		return models.ErrResourceNotFound
+	}
+	return nil
+}
+
+func (db *SQLDB) fetchDesiredLRP(logger lager.Logger, scanner RowScanner) (*models.DesiredLRP, error) {
+	var desiredLRP models.DesiredLRP
+	desiredLRP.ModificationTag = &models.ModificationTag{}
+	var routeData, runInformationData []byte
+	err := scanner.Scan(
+		&desiredLRP.ProcessGuid,
+		&desiredLRP.Domain,
+		&desiredLRP.LogGuid,
+		&desiredLRP.Annotation,
+		&desiredLRP.Instances,
+		&desiredLRP.MemoryMb,
+		&desiredLRP.DiskMb,
+		&desiredLRP.RootFs,
+		&routeData,
+		&desiredLRP.ModificationTag.Epoch,
+		&desiredLRP.ModificationTag.Index,
+		&runInformationData,
+	)
+	if err != nil {
+		return nil, models.ErrResourceNotFound
+	}
+
+	var runInformation models.DesiredLRPRunInfo
+	err = db.serializer.Unmarshal(logger, runInformationData, &runInformation)
+	if err != nil {
+		return nil, err
+	}
+	desiredLRP.AddRunInfo(runInformation)
+
+	var routes models.Routes
+	err = json.Unmarshal(routeData, &routes)
+	if err != nil {
+		return nil, models.ErrDeserializeJSON
+	}
+
+	desiredLRP.Routes = &routes
+
+	return &desiredLRP, nil
+}
+
+func (db *SQLDB) fetchDesiredLRPSchedulingInfo(logger lager.Logger, scanner RowScanner) (*models.DesiredLRPSchedulingInfo, error) {
+	var desiredLRPSchedulingInfo models.DesiredLRPSchedulingInfo
+	desiredLRPSchedulingInfo.ModificationTag = models.ModificationTag{}
+	var routeData []byte
+	err := scanner.Scan(
+		&desiredLRPSchedulingInfo.ProcessGuid,
+		&desiredLRPSchedulingInfo.Domain,
+		&desiredLRPSchedulingInfo.LogGuid,
+		&desiredLRPSchedulingInfo.Annotation,
+		&desiredLRPSchedulingInfo.Instances,
+		&desiredLRPSchedulingInfo.MemoryMb,
+		&desiredLRPSchedulingInfo.DiskMb,
+		&desiredLRPSchedulingInfo.RootFs,
+		&routeData,
+		&desiredLRPSchedulingInfo.ModificationTag.Epoch,
+		&desiredLRPSchedulingInfo.ModificationTag.Index,
+	)
+	if err != nil {
+		return nil, models.ErrResourceNotFound
+	}
+
+	var routes models.Routes
+	err = json.Unmarshal(routeData, &routes)
+	if err != nil {
+		return nil, models.ErrDeserializeJSON
+	}
+
+	desiredLRPSchedulingInfo.Routes = routes
+
+	return &desiredLRPSchedulingInfo, nil
 }
--- db/sqldb/desired_lrp_db_test.go
+++ db/sqldb/desired_lrp_db_test.go
@@ -2,6 +2,7 @@ package sqldb_test
 
 import (
 	"encoding/json"
+	"fmt"
 
 	"github.com/cloudfoundry-incubator/bbs/models"
 	"github.com/cloudfoundry-incubator/bbs/models/test/model_helpers"
@@ -78,6 +79,280 @@ var _ = Describe("DesiredLRPDB", func() {
 			It("returns a resource exists error", func() {
 				err := sqlDB.DesireLRP(logger, expectedDesiredLRP)
 				Expect(err).To(Equal(models.ErrResourceExists))
+			})
+		})
+	})
+
+	Describe("DesiredLRPByProcessGuid", func() {
+		var expectedDesiredLRP *models.DesiredLRP
+
+		BeforeEach(func() {
+			desiredLRPGuid := "desired-lrp-guid"
+			expectedDesiredLRP = model_helpers.NewValidDesiredLRP(desiredLRPGuid)
+			Expect(sqlDB.DesireLRP(logger, expectedDesiredLRP)).To(Succeed())
+		})
+
+		It("returns the desired lrp", func() {
+			desiredLRP, err := sqlDB.DesiredLRPByProcessGuid(logger, expectedDesiredLRP.ProcessGuid)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(desiredLRP).To(BeEquivalentTo(expectedDesiredLRP))
+		})
+
+		Context("when the desired lrp does not exist", func() {
+			It("returns a ResourceNotFound error", func() {
+				desiredLRP, err := sqlDB.DesiredLRPByProcessGuid(logger, "Sup dawg")
+				Expect(err).To(Equal(models.ErrResourceNotFound))
+				Expect(desiredLRP).To(BeNil())
+			})
+		})
+
+		Context("when the run info is invalid", func() {
+			BeforeEach(func() {
+				result, err := db.Exec("UPDATE desired_lrps SET run_info = ? WHERE process_guid = ?", "{{", expectedDesiredLRP.ProcessGuid)
+				Expect(err).NotTo(HaveOccurred())
+				rowsAffected, err := result.RowsAffected()
+				Expect(err).NotTo(HaveOccurred())
+				Expect(rowsAffected).To(BeEquivalentTo(1))
+			})
+
+			It("returns an invalid record error", func() {
+				desiredLRP, err := sqlDB.DesiredLRPByProcessGuid(logger, expectedDesiredLRP.ProcessGuid)
+				Expect(err).To(HaveOccurred())
+				Expect(desiredLRP).To(BeNil())
+			})
+		})
+
+		Context("when the routes are invalid", func() {
+			BeforeEach(func() {
+				result, err := db.Exec("UPDATE desired_lrps SET routes = ? WHERE process_guid = ?", "{{", expectedDesiredLRP.ProcessGuid)
+				Expect(err).NotTo(HaveOccurred())
+				rowsAffected, err := result.RowsAffected()
+				Expect(err).NotTo(HaveOccurred())
+				Expect(rowsAffected).To(BeEquivalentTo(1))
+			})
+
+			It("returns an invalid record error", func() {
+				desiredLRP, err := sqlDB.DesiredLRPByProcessGuid(logger, expectedDesiredLRP.ProcessGuid)
+				Expect(err).To(HaveOccurred())
+				Expect(desiredLRP).To(BeNil())
+			})
+		})
+	})
+
+	Describe("DesiredLRPs", func() {
+		var expectedDesiredLRPs []*models.DesiredLRP
+
+		BeforeEach(func() {
+			expectedDesiredLRPs = []*models.DesiredLRP{}
+			expectedDesiredLRPs = append(expectedDesiredLRPs, model_helpers.NewValidDesiredLRP("d-1"))
+			expectedDesiredLRPs = append(expectedDesiredLRPs, model_helpers.NewValidDesiredLRP("d-2"))
+			for i, expectedDesiredLRP := range expectedDesiredLRPs {
+				expectedDesiredLRP.Domain = fmt.Sprintf("domain-%d", i+1)
+				Expect(sqlDB.DesireLRP(logger, expectedDesiredLRP)).To(Succeed())
+			}
+		})
+
+		It("returns all desired lrps", func() {
+			desiredLRPs, err := sqlDB.DesiredLRPs(logger, models.DesiredLRPFilter{})
+			Expect(err).NotTo(HaveOccurred())
+			Expect(desiredLRPs).To(HaveLen(2))
+			Expect(desiredLRPs).To(ConsistOf(expectedDesiredLRPs))
+		})
+
+		Context("when filtering by domain", func() {
+			It("returns a ResourceNotFound error", func() {
+				desiredLRPs, err := sqlDB.DesiredLRPs(logger, models.DesiredLRPFilter{Domain: "domain-1"})
+				Expect(err).NotTo(HaveOccurred())
+				Expect(desiredLRPs).To(HaveLen(1))
+				Expect(desiredLRPs[0]).To(BeEquivalentTo(expectedDesiredLRPs[0]))
+			})
+		})
+
+		Context("when the run info is invalid", func() {
+			BeforeEach(func() {
+				result, err := db.Exec("UPDATE desired_lrps SET run_info = ? WHERE process_guid = ?", "{{", expectedDesiredLRPs[0].ProcessGuid)
+				Expect(err).NotTo(HaveOccurred())
+				rowsAffected, err := result.RowsAffected()
+				Expect(err).NotTo(HaveOccurred())
+				Expect(rowsAffected).To(BeEquivalentTo(1))
+			})
+
+			It("excludes the invalid desired LRP from the response", func() {
+				desiredLRPs, err := sqlDB.DesiredLRPs(logger, models.DesiredLRPFilter{})
+				Expect(err).NotTo(HaveOccurred())
+				Expect(desiredLRPs).To(HaveLen(1))
+			})
+		})
+
+		Context("when the routes are invalid", func() {
+			BeforeEach(func() {
+				result, err := db.Exec("UPDATE desired_lrps SET routes = ? WHERE process_guid = ?", "{{", expectedDesiredLRPs[0].ProcessGuid)
+				Expect(err).NotTo(HaveOccurred())
+				rowsAffected, err := result.RowsAffected()
+				Expect(err).NotTo(HaveOccurred())
+				Expect(rowsAffected).To(BeEquivalentTo(1))
+			})
+
+			It("excludes the invalid desired LRP from the response", func() {
+				desiredLRPs, err := sqlDB.DesiredLRPs(logger, models.DesiredLRPFilter{})
+				Expect(err).NotTo(HaveOccurred())
+				Expect(desiredLRPs).To(HaveLen(1))
+			})
+		})
+	})
+
+	Describe("DesiredLRPSchedulingInfos", func() {
+		var expectedDesiredLRPSchedulingInfos []*models.DesiredLRPSchedulingInfo
+		var expectedDesiredLRPs []*models.DesiredLRP
+
+		BeforeEach(func() {
+			expectedDesiredLRPs = []*models.DesiredLRP{}
+			expectedDesiredLRPSchedulingInfos = []*models.DesiredLRPSchedulingInfo{}
+			expectedDesiredLRPs = append(expectedDesiredLRPs, model_helpers.NewValidDesiredLRP("d-1"))
+			expectedDesiredLRPs = append(expectedDesiredLRPs, model_helpers.NewValidDesiredLRP("d-2"))
+			for i, expectedDesiredLRP := range expectedDesiredLRPs {
+				expectedDesiredLRP.Domain = fmt.Sprintf("domain-%d", i+1)
+				Expect(sqlDB.DesireLRP(logger, expectedDesiredLRP)).To(Succeed())
+				schedulingInfo := expectedDesiredLRP.DesiredLRPSchedulingInfo()
+				expectedDesiredLRPSchedulingInfos = append(expectedDesiredLRPSchedulingInfos, &schedulingInfo)
+			}
+		})
+
+		It("returns all desired lrps scheduling infos", func() {
+			desiredLRPSchedulingInfos, err := sqlDB.DesiredLRPSchedulingInfos(logger, models.DesiredLRPFilter{})
+			Expect(err).NotTo(HaveOccurred())
+			Expect(desiredLRPSchedulingInfos).To(HaveLen(2))
+			Expect(desiredLRPSchedulingInfos).To(ConsistOf(expectedDesiredLRPSchedulingInfos))
+		})
+
+		Context("when filtering by domain", func() {
+			It("returns a ResourceNotFound error", func() {
+				desiredLRPSchedulingInfos, err := sqlDB.DesiredLRPSchedulingInfos(logger, models.DesiredLRPFilter{Domain: "domain-1"})
+				Expect(err).NotTo(HaveOccurred())
+				Expect(desiredLRPSchedulingInfos).To(HaveLen(1))
+				Expect(desiredLRPSchedulingInfos[0]).To(BeEquivalentTo(expectedDesiredLRPSchedulingInfos[0]))
+			})
+		})
+
+		Context("when the routes are invalid", func() {
+			BeforeEach(func() {
+				result, err := db.Exec("UPDATE desired_lrps SET routes = ? WHERE process_guid = ?", "{{", expectedDesiredLRPs[0].ProcessGuid)
+				Expect(err).NotTo(HaveOccurred())
+				rowsAffected, err := result.RowsAffected()
+				Expect(err).NotTo(HaveOccurred())
+				Expect(rowsAffected).To(BeEquivalentTo(1))
+			})
+
+			It("excludes the invalid desired LRP from the response", func() {
+				desiredLRPSchedulingInfos, err := sqlDB.DesiredLRPSchedulingInfos(logger, models.DesiredLRPFilter{})
+				Expect(err).NotTo(HaveOccurred())
+				Expect(desiredLRPSchedulingInfos).To(HaveLen(1))
+			})
+		})
+	})
+
+	Describe("UpdateDesiredLRP", func() {
+		var expectedDesiredLRP *models.DesiredLRP
+		var update *models.DesiredLRPUpdate
+
+		BeforeEach(func() {
+			desiredLRPGuid := "desired-lrp-guid"
+			expectedDesiredLRP = model_helpers.NewValidDesiredLRP(desiredLRPGuid)
+			Expect(sqlDB.DesireLRP(logger, expectedDesiredLRP)).To(Succeed())
+			instances := int32(1)
+			update = &models.DesiredLRPUpdate{
+				Instances: &instances,
+			}
+		})
+
+		It("updates the lrp", func() {
+			instances := int32(123)
+			routeContent := []byte("{}")
+			routes := models.Routes{
+				"blah": (*json.RawMessage)(&routeContent),
+			}
+			annotation := "annotated"
+			update = &models.DesiredLRPUpdate{
+				Instances:  &instances,
+				Routes:     &routes,
+				Annotation: &annotation,
+			}
+			err := sqlDB.UpdateDesiredLRP(logger, expectedDesiredLRP.ProcessGuid, update)
+			Expect(err).NotTo(HaveOccurred())
+
+			desiredLRP, err := sqlDB.DesiredLRPByProcessGuid(logger, expectedDesiredLRP.ProcessGuid)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(desiredLRP).To(BeEquivalentTo(expectedDesiredLRP.ApplyUpdate(update)))
+		})
+
+		It("updates only the fields in the update parameter", func() {
+			instances := int32(20)
+			update = &models.DesiredLRPUpdate{
+				Instances: &instances,
+			}
+			err := sqlDB.UpdateDesiredLRP(logger, expectedDesiredLRP.ProcessGuid, update)
+			Expect(err).NotTo(HaveOccurred())
+
+			desiredLRP, err := sqlDB.DesiredLRPByProcessGuid(logger, expectedDesiredLRP.ProcessGuid)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(desiredLRP).To(BeEquivalentTo(expectedDesiredLRP.ApplyUpdate(update)))
+		})
+
+		It("does nothing if update is empty", func() {
+			update = &models.DesiredLRPUpdate{}
+			err := sqlDB.UpdateDesiredLRP(logger, expectedDesiredLRP.ProcessGuid, update)
+			Expect(err).NotTo(HaveOccurred())
+
+			desiredLRP, err := sqlDB.DesiredLRPByProcessGuid(logger, expectedDesiredLRP.ProcessGuid)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(desiredLRP).To(BeEquivalentTo(expectedDesiredLRP.ApplyUpdate(update)))
+		})
+
+		Context("when routes param is invalid", func() {
+			It("returns a bad request error", func() {
+				routeContent := []byte("bad json")
+				routes := models.Routes{
+					"blah": (*json.RawMessage)(&routeContent),
+				}
+				update = &models.DesiredLRPUpdate{
+					Routes: &routes,
+				}
+				err := sqlDB.UpdateDesiredLRP(logger, expectedDesiredLRP.ProcessGuid, update)
+				Expect(err).To(HaveOccurred())
+				Expect(err).To(Equal(models.ErrBadRequest))
+			})
+		})
+
+		Context("when the desired lrp does not exist", func() {
+			It("returns a ResourceNotFound error", func() {
+				err := sqlDB.UpdateDesiredLRP(logger, "does-not-exist", update)
+				Expect(err).To(Equal(models.ErrResourceNotFound))
+			})
+		})
+	})
+
+	Describe("RemoveDesiredLRP", func() {
+		var expectedDesiredLRP *models.DesiredLRP
+
+		BeforeEach(func() {
+			desiredLRPGuid := "desired-lrp-guid"
+			expectedDesiredLRP = model_helpers.NewValidDesiredLRP(desiredLRPGuid)
+			Expect(sqlDB.DesireLRP(logger, expectedDesiredLRP)).To(Succeed())
+		})
+
+		It("removes the lrp", func() {
+			err := sqlDB.RemoveDesiredLRP(logger, expectedDesiredLRP.ProcessGuid)
+			Expect(err).NotTo(HaveOccurred())
+
+			_, err = sqlDB.DesiredLRPByProcessGuid(logger, expectedDesiredLRP.ProcessGuid)
+			Expect(err).To(HaveOccurred())
+			Expect(err).To(Equal(models.ErrResourceNotFound))
+		})
+
+		Context("when the desired lrp does not exist", func() {
+			It("returns a ResourceNotFound error", func() {
+				err := sqlDB.RemoveDesiredLRP(logger, "does-not-exist")
+				Expect(err).To(Equal(models.ErrResourceNotFound))
 			})
 		})
 	})
