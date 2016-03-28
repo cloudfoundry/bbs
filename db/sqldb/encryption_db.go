2cfd4bfa329479136804127dfc41c65603a24c19
Andrew Edgar <aedgar@ca.ibm.com>
2016-03-28 14:30:50 -0700

    Standardize logging in encryption_db
    Signed-off-by: Jen Spinney <jen.spinney@hpe.com>

--- db/sqldb/encryption_db.go
+++ db/sqldb/encryption_db.go
@@ -5,9 +5,17 @@ import "github.com/pivotal-golang/lager"
 const EncryptionKeyID = "encryption_key_label"
 
 func (db *SQLDB) SetEncryptionKeyLabel(logger lager.Logger, label string) error {
+	logger = logger.Session("set-encrption-key-label", lager.Data{"label": label})
+	logger.Debug("starting")
+	defer logger.Debug("complete")
+
 	return db.setConfigurationValue(logger, EncryptionKeyID, label)
 }
 
 func (db *SQLDB) EncryptionKeyLabel(logger lager.Logger) (string, error) {
+	logger = logger.Session("encrption-key-label")
+	logger.Debug("starting")
+	defer logger.Debug("complete")
+
 	return db.getConfigurationValue(logger, EncryptionKeyID)
 }
