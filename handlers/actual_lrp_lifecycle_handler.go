66d9f2b65bfb0afa04bd1d925fc4d6b1150159bc
Luan Santos <lsantos@pivotal.io>
2016-04-29 11:14:36 -0600

    Emit CrashedEvent from CrashActualLRP calls in handlers
    [#116637751]
    
    Signed-off-by: Andrew Edgar <aedgar@ca.ibm.com>

--- handlers/actual_lrp_lifecycle_handler.go
+++ handlers/actual_lrp_lifecycle_handler.go
@@ -130,6 +130,8 @@ func (h *ActualLRPLifecycleHandler) CrashActualLRP(w http.ResponseWriter, req *h
 		}
 	}
 
+	actualLRP, _ := after.Resolve()
+	go h.actualHub.Emit(models.NewActualLRPCrashedEvent(actualLRP))
 	go h.actualHub.Emit(models.NewActualLRPChangedEvent(before, after))
 }
 
