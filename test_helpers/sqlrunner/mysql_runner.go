a3274226645f71db9d2d0b6293cd4b0f25dd413e
Jen Spinney <jen.spinney@hpe.com>
2016-06-09 12:13:36 -0700

    Move sqlrunner out of storeadapter and into the bbs
    [#119290575]
    
    Signed-off-by: James Myers <jmyers@pivotal.io>

---
+++ test_helpers/sqlrunner/mysql_runner.go
@@ -0,0 +1,87 @@
+package sqlrunner
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	"github.com/go-sql-driver/mysql"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+// MySQLRunner is responsible for creating and tearing down a test database in
+// a local MySQL instance. This runner assumes mysql is already running
+// locally, and does not start or stop the mysql service.  mysql must be set up
+// on localhost as described in the CONTRIBUTING.md doc in diego-release.
+type MySQLRunner struct {
+	sqlDBName string
+	db        *sql.DB
+}
+
+func NewMySQLRunner(sqlDBName string) *MySQLRunner {
+	return &MySQLRunner{
+		sqlDBName: sqlDBName,
+	}
+}
+
+func (m *MySQLRunner) Run(signals <-chan os.Signal, ready chan<- struct{}) error {
+	defer GinkgoRecover()
+
+	var err error
+	m.db, err = sql.Open("mysql", "diego:diego_password@/")
+	Expect(err).NotTo(HaveOccurred())
+	Expect(m.db.Ping()).NotTo(HaveOccurred())
+
+	_, err = m.db.Exec(fmt.Sprintf("CREATE DATABASE %s", m.sqlDBName))
+	Expect(err).NotTo(HaveOccurred())
+
+	m.db, err = sql.Open("mysql", fmt.Sprintf("diego:diego_password@/%s", m.sqlDBName))
+	Expect(err).NotTo(HaveOccurred())
+	Expect(m.db.Ping()).NotTo(HaveOccurred())
+
+	close(ready)
+
+	<-signals
+
+	_, err = m.db.Exec(fmt.Sprintf("DROP DATABASE %s", m.sqlDBName))
+	Expect(err).NotTo(HaveOccurred())
+	Expect(m.db.Close()).To(Succeed())
+
+	return nil
+}
+
+func (m *MySQLRunner) ConnectionString() string {
+	return fmt.Sprintf("diego:diego_password@/%s", m.sqlDBName)
+}
+
+func (m *MySQLRunner) DriverName() string {
+	return "mysql"
+}
+
+func (m *MySQLRunner) DB() *sql.DB {
+	return m.db
+}
+
+func (m *MySQLRunner) Reset() {
+	var truncateTablesSQL = []string{
+		"TRUNCATE TABLE domains",
+		"TRUNCATE TABLE configurations",
+		"TRUNCATE TABLE tasks",
+		"TRUNCATE TABLE desired_lrps",
+		"TRUNCATE TABLE actual_lrps",
+	}
+	for _, query := range truncateTablesSQL {
+		result, err := m.db.Exec(query)
+		switch err := err.(type) {
+		case *mysql.MySQLError:
+			if err.Number == 1146 {
+				// missing table error, it's fine because we're trying to truncate it
+				continue
+			}
+		}
+
+		Expect(err).NotTo(HaveOccurred())
+		Expect(result.RowsAffected()).To(BeEquivalentTo(0))
+	}
+}
---
+++ test_helpers/sqlrunner/postgres_runner.go
@@ -0,0 +1,93 @@
+package sqlrunner
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	"github.com/lib/pq"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+// PostgresRunner is responsible for creating and tearing down a test database in
+// a local Postgres instance. This runner assumes mysql is already running
+// locally, and does not start or stop the mysql service.  mysql must be set up
+// on localhost as described in the CONTRIBUTING.md doc in diego-release.
+type PostgresRunner struct {
+	db        *sql.DB
+	sqlDBName string
+}
+
+func NewPostgresRunner(sqlDBName string) *PostgresRunner {
+	return &PostgresRunner{
+		sqlDBName: sqlDBName,
+	}
+}
+
+func (p *PostgresRunner) Run(signals <-chan os.Signal, ready chan<- struct{}) error {
+	defer GinkgoRecover()
+
+	var err error
+	p.db, err = sql.Open("postgres", "postgres://diego:diego_pw@localhost")
+	Expect(err).NotTo(HaveOccurred())
+	Expect(p.db.Ping()).NotTo(HaveOccurred())
+
+	_, err = p.db.Exec(fmt.Sprintf("CREATE DATABASE %s", p.sqlDBName))
+	Expect(err).NotTo(HaveOccurred())
+
+	p.db, err = sql.Open("postgres", fmt.Sprintf("postgres://diego:diego_pw@localhost/%s", p.sqlDBName))
+	Expect(err).NotTo(HaveOccurred())
+	Expect(p.db.Ping()).NotTo(HaveOccurred())
+
+	close(ready)
+
+	<-signals
+
+	// We need to close the connection to the database we want to drop before dropping it.
+	p.db.Close()
+	p.db, err = sql.Open("postgres", "postgres://diego:diego_pw@localhost")
+	Expect(err).NotTo(HaveOccurred())
+
+	_, err = p.db.Exec(fmt.Sprintf("DROP DATABASE %s", p.sqlDBName))
+	Expect(err).NotTo(HaveOccurred())
+	Expect(p.db.Close()).To(Succeed())
+
+	return nil
+}
+
+func (p *PostgresRunner) ConnectionString() string {
+	return fmt.Sprintf("postgres://diego:diego_pw@localhost/%s", p.sqlDBName)
+}
+
+func (p *PostgresRunner) DriverName() string {
+	return "postgres"
+}
+
+func (p *PostgresRunner) DB() *sql.DB {
+	return p.db
+}
+
+func (p *PostgresRunner) Reset() {
+	var truncateTablesSQL = []string{
+		"TRUNCATE TABLE domains",
+		"TRUNCATE TABLE configurations",
+		"TRUNCATE TABLE tasks",
+		"TRUNCATE TABLE desired_lrps",
+		"TRUNCATE TABLE actual_lrps",
+	}
+	for _, query := range truncateTablesSQL {
+		result, err := p.db.Exec(query)
+
+		switch err := err.(type) {
+		case *pq.Error:
+			if err.Code == "42P01" {
+				// missing table error, it's fine because we're trying to truncate it
+				continue
+			}
+		}
+
+		Expect(err).NotTo(HaveOccurred())
+		Expect(result.RowsAffected()).To(BeEquivalentTo(0))
+	}
+}
