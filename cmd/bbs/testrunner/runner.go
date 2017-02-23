af2a4981537e69445a78e686dace833cbf22c5fb
Anoop Gopalakrishnan <agopalakrishnan@pivotal.io>
2017-02-23 15:42:02 -0800

    bump the start check timeout for the testrunner
    [#140531263]
    
    Signed-off-by: Nima Kaviani <nkavian@us.ibm.com>

--- cmd/bbs/testrunner/runner.go
+++ cmd/bbs/testrunner/runner.go
@@ -26,9 +26,10 @@ func New(binPath string, bbsConfig config.BBSConfig) *ginkgomon.Runner {
 	Expect(err).NotTo(HaveOccurred())
 
 	return ginkgomon.New(ginkgomon.Config{
-		Name:       "bbs",
-		Command:    exec.Command(binPath, "-config", f.Name()),
-		StartCheck: "bbs.started",
+		Name:              "bbs",
+		Command:           exec.Command(binPath, "-config", f.Name()),
+		StartCheck:        "bbs.started",
+		StartCheckTimeout: 10 * time.Second,
 		Cleanup: func() {
 			// do not use Expect otherwise a race condition will happen
 			os.RemoveAll(f.Name())
