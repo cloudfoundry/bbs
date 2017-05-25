9bd8ba7e034c5b4e81a2cfbaa11796d449745cef
John Shahid <jvshahid@gmail.com>
2017-05-25 11:09:52 -0400

    Regenerate protobuf files
    [#145913949]

--- scripts/generate_protos.sh
+++ scripts/generate_protos.sh
@@ -1,7 +1,4 @@
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  echo "OSX workstation detected, updating protobuf binaries..."
-  rm $GOPATH/bin/protoc-gen-gogoslick
-  go install -v github.com/gogo/protobuf/protoc-gen-gogoslick
-fi
-
-protoc --proto_path=$GOPATH/src:$GOPATH/src/github.com/gogo/protobuf/protobuf/:. --gogoslick_out=. *.proto
+rm $GOPATH/bin/protoc-gen-gogoslick
+go install -v github.com/gogo/protobuf/protoc-gen-gogoslick
+echo "Generating pb.go files"
+protoc --proto_path=$GOPATH/src:$GOPATH/src/github.com/gogo/protobuf/protobuf/:$GOPATH/src/github.com/golang/protobuf/ptypes/duration/:. --gogoslick_out=plugins=grpc:. *.proto
