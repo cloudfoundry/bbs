ae0ad34dfda95446f13b2df9c4b4677ce6f11ad0
Amin Jamali <ajamali@pivotal.io>
2021-06-02 19:06:46 +0000

    fix generate_protos scripts
    remove un-needed imports

--- scripts/generate_protos.sh
+++ scripts/generate_protos.sh
@@ -1,4 +1,4 @@
-rm $GOPATH/bin/protoc-gen-gogoslick
-go install -v github.com/gogo/protobuf/protoc-gen-gogoslick
-echo "Generating pb.go files"
-protoc --proto_path=$GOPATH/src:$GOPATH/src/github.com/gogo/protobuf/protobuf/:$GOPATH/src/github.com/golang/protobuf/ptypes/duration/:. --gogoslick_out=plugins=grpc:. *.proto
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+pushd "$DIR/../models"
+protoc --proto_path=$DIEGO_RELEASE_DIR/src/code.cloudfoundry.org/vendor:$DIEGO_RELEASE_DIR/src/code.cloudfoundry.org/vendor/github.com/golang/protobuf/ptypes/duration/:. --gogoslick_out=plugins=grpc:. *.proto
+popd
