1250973e826c4c790fb293e56019754125937594
Geoff Franks <gfranks@vmware.com>
2023-03-29 09:47:18 -0400

    Bump sqldb/helpers + test helpers to lager v3 (#61)
    Co-authored-by: Amin Jamali <71907054+winkingturtle-vmw@users.noreply.github.com>

--- db/sqldb/helpers/helpers_suite_test.go
+++ db/sqldb/helpers/helpers_suite_test.go
@@ -8,7 +8,7 @@ import (
 
 	"code.cloudfoundry.org/bbs/db/sqldb/helpers"
 	"code.cloudfoundry.org/bbs/test_helpers"
-	"code.cloudfoundry.org/lager"
+	"code.cloudfoundry.org/lager/v3"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
