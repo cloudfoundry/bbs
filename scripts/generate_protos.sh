0565a9dab863f49e1e2ed6c12227e89a1a6082fd
Renee Chu <reneec@vmware.com>
2023-08-26 00:07:30 +0000

    first start at updating script to use protoc-gen-go

--- scripts/generate_protos.sh
+++ scripts/generate_protos.sh
@@ -1,4 +1,6 @@
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-pushd "$DIR/../models"
-protoc --proto_path=$DIEGO_RELEASE_DIR/src/code.cloudfoundry.org/vendor:$DIEGO_RELEASE_DIR/src/code.cloudfoundry.org/vendor/github.com/golang/protobuf/ptypes/duration/:. --gogoslick_out=plugins=grpc:. *.proto
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )" 
+pushd "$DIR/../models" 
+#protoc --proto_path=$DIEGO_RELEASE_DIR/src/code.cloudfoundry.org/vendor:$DIEGO_RELEASE_DIR/src/code.cloudfoundry.org/vendor/github.com/golang/protobuf/ptypes/duration/:. --gogoslick_out=plugins=grpc:. *.proto 
+protoc  --go_out=plugins=grpc:. *.proto 
+
 popd
