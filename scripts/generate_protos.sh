cb2be73b3da4f449d497d2dc3e61da1440dc0778
Renee Chu <reneec@vmware.com>
2023-08-28 22:58:01 +0000

    generated pb.go models from plugin update
    Signed-off-by: Marc Paquette <mpaquette@vmware.com>

--- scripts/generate_protos.sh
+++ scripts/generate_protos.sh
@@ -1,6 +1,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )" 
 pushd "$DIR/../models" 
 #protoc --proto_path=$DIEGO_RELEASE_DIR/src/code.cloudfoundry.org/vendor:$DIEGO_RELEASE_DIR/src/code.cloudfoundry.org/vendor/github.com/golang/protobuf/ptypes/duration/:. --gogoslick_out=plugins=grpc:. *.proto 
-protoc  --go_out=plugins=grpc:. *.proto 
-
+protoc  --go_out=.  --go_opt=paths=source_relative  *.proto 
 popd
