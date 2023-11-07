c3145ec28942f7c1a037c1cb1705ea3112a3bc59
Marc Paquette <mpaquette@vmware.com>
2023-11-07 15:57:32 +0000

    set -e to exit when something fails
    Signed-off-by: Amin Jamali <ajamali@vmware.com>

--- scripts/generate_protos.sh
+++ scripts/generate_protos.sh
@@ -1,3 +1,4 @@
+set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 pushd "$DIR/../models"
 protoc --proto_path=../../vendor:../../vendor/github.com/golang/protobuf/ptypes/duration/:. --gogoslick_out=plugins=grpc:. *.proto
