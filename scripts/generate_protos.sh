c6cf6e6bb46b21414c28cd9390185f26a109d97a
Amin Jamali <ajamali@vmware.com>
2023-11-07 15:54:52 +0000

    Fix path
    Signed-off-by: Marc Paquette <mpaquette@vmware.com>

--- scripts/generate_protos.sh
+++ scripts/generate_protos.sh
@@ -1,4 +1,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 pushd "$DIR/../models"
-protoc --proto_path=../../src/code.cloudfoundry.org/vendor:../../src/code.cloudfoundry.org/vendor/github.com/golang/protobuf/ptypes/duration/:. --gogoslick_out=plugins=grpc:. *.proto
+protoc --proto_path=../../vendor:../../vendor/github.com/golang/protobuf/ptypes/duration/:. --gogoslick_out=plugins=grpc:. *.proto
 popd
