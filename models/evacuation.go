4213dc9d250cb5d5584f268880c376c6c58303a1
Brandon Roberson <brandon.roberson@broadcom.com>
2024-05-02 16:20:34 +0000

    Remove duplicate methods generated by plugin

--- models/evacuation.go
+++ models/evacuation.go
@@ -1,12 +1,1 @@
 package models
-
-func (request *EvacuateRunningActualLRPRequest) SetRoutable(routable bool) {
-	request.OptionalRoutable = &EvacuateRunningActualLRPRequest_Routable{
-		Routable: routable,
-	}
-}
-
-func (request *EvacuateRunningActualLRPRequest) RoutableExists() bool {
-	_, ok := request.GetOptionalRoutable().(*EvacuateRunningActualLRPRequest_Routable)
-	return ok
-}
