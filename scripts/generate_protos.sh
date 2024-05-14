29a77d25278be54f15fc1f3bca8e734e6d791783
Brandon Roberson <brandon.roberson@broadcom.com>
2024-05-14 17:12:43 +0000

    Update generate_protos script to handle plugin

--- scripts/generate_protos.sh
+++ scripts/generate_protos.sh
@@ -1,5 +1,17 @@
+#! /bin/bash
 set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-pushd "$DIR/../models"
-protoc --proto_path=../../vendor:../../vendor/github.com/golang/protobuf/ptypes/duration/:. --gogoslick_out=plugins=grpc:. *.proto
+# regenerate protos for protoc plugin
+pushd "${DIR}/../protoc-gen-go-bbs"
+  protoc --proto_path=. --go_out=. --go_opt=paths=source_relative ./*.proto
+  # we need the custom bbs code from the plugin because of references made by some of the model protos
+  cp ./bbs.pb.go "${DIR}/../models/"
+
+  # we also need to change the package after it's been copied away
+  sed -i 's/package models/package main/g' ./bbs.pb.go
+popd
+
+# regenerate protos for models
+pushd "${DIR}/../models"
+  protoc --proto_path=.:../protoc-gen-go-bbs --go_out=. --go-bbs_out=. --go_opt=paths=source_relative --go-bbs_opt=paths=source_relative ./*.proto 
 popd
