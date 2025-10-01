f6a32e0a399e6c1e94768a21030b1ca708b1f27a
karthick udayakumar <karthick.udayakumar@broadcom.com>
2025-10-01 12:01:13 -0400

    add metron helper for logging client

--- test_helpers/metronserver_setup.go
+++ test_helpers/metronserver_setup.go
@@ -1,11 +1,15 @@
 package test_helpers
 
 import (
-	"path"
-
 	loggingclient "code.cloudfoundry.org/diego-logging-client"
 	"code.cloudfoundry.org/diego-logging-client/testhelpers"
 	"code.cloudfoundry.org/go-loggregator/v9/rpc/loggregator_v2"
+)
+
+const (
+	MetronCAFile         = "fixtures/metron/CA.crt"
+	MetronServerCertFile = "fixtures/metron/metron.crt"
+	MetronServerKeyFile  = "fixtures/metron/metron.key"
 )
 
 type MetronIngressSetup struct {
@@ -16,11 +20,7 @@ type MetronIngressSetup struct {
 }
 
 func StartMetronIngress(fixturesPath string) (*MetronIngressSetup, error) {
-	metronCAFile := path.Join(fixturesPath, "metron", "CA.crt")
-	metronServerCertFile := path.Join(fixturesPath, "metron", "metron.crt")
-	metronServerKeyFile := path.Join(fixturesPath, "metron", "metron.key")
-
-	testIngressServer, err := testhelpers.NewTestIngressServer(metronServerCertFile, metronServerKeyFile, metronCAFile)
+	testIngressServer, err := testhelpers.NewTestIngressServer(MetronServerCertFile, MetronServerKeyFile, MetronCAFile)
 	if err != nil {
 		return nil, err
 	}
@@ -46,8 +46,8 @@ func StartMetronIngress(fixturesPath string) (*MetronIngressSetup, error) {
 
 func GetLoggregatorConfigWithMetronCerts() loggingclient.Config {
 	return loggingclient.Config{
-		CACertPath: "fixtures/metron/CA.crt",
-		CertPath:   "fixtures/metron/metron.crt",
-		KeyPath:    "fixtures/metron/metron.key",
+		CACertPath: MetronCAFile,
+		CertPath:   MetronServerCertFile,
+		KeyPath:    MetronServerKeyFile,
 	}
 }
